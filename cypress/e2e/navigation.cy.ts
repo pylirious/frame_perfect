80de99fbf25a1b342f9d12b8a4add47c5534221c
pylirious <samuel-p@outlook.de>
2022-12-20 02:25:40 +0100

    added navigation tests

---
+++ cypress/e2e/navigation.cy.ts
@@ -0,0 +1,36 @@
+describe('Navigation to minecraft page', () => {
+  it('should navigate to the minecraft records page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    // Find a link with an href attribute containing "games" and click it
+    cy.get('a[href*="games"]').click()
+
+    // Find a link with an href attribute containing "minecraft" and click it
+    cy.get('a[href*="minecraft"]').click()
+
+
+    // The new url should include "/games"
+    cy.url().should('include', '/minecraft')
+
+    // The new page should contain an h1 with "Speedruns"
+    cy.get('h3').contains('Minecraft')
+  })
+})
+
+describe('Navigation to speedruns', () => {
+  it('should navigate to the speedruns page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    // Find a link with an href attribute containing "speedruns" and click it
+    cy.get('a[href*="speedruns"]').click()
+
+    // The new url should include "/speedruns"
+    cy.url().should('include', '/speedruns')
+
+    // The new page should contain an h1 with "Speedruns"
+    cy.get('h1').contains('Speedruns')
+  })
+})
+
